c61cd91f2ba95475c4df4e85886b8055aea81302
mohosin2126 <mohosin2126@gmail.com>
2024-11-24 22:25:02 +0600

    not found has been solved

---
+++ 404-not-found.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    if (x == 404)
+    {
+        printf("NOT FOUND");
+    }
+    else
+    {
+        printf("Found");
+    }
+    return 0;
+}
