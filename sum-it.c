8e22e1d4478f07515de50f6eee924117f18f2ada
mohosin2126 <mohosin2126@gmail.com>
2024-11-27 10:08:47 +0600

    sum it has been solved

---
+++ sum-it.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int t, a, b, c;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+
+        if (a + b == c)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
