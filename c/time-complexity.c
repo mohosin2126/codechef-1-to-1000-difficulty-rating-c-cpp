99bc82e1e8f1ef71bbf29aeda55c62cd03351e36
mohosin2126 <mohosin2126@gmail.com>
2024-11-30 23:14:15 +0600

    time complexity has been solved

---
+++ c/time-complexity.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        if (x > y)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
