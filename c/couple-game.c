cc327fb1b0a1847e1f8b19b892ef5ab10e79566f
mohosin2126 <mohosin2126@gmail.com>
2024-11-30 23:04:23 +0600

    couple game has been solved

---
+++ c/couple-game.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        printf("%d\n", y - x);
+    }
+    return 0;
+}
