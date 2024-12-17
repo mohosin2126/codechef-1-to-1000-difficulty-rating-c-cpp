61b6fd306983504273f579d78004a45eec3d728f
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2024-12-17 21:39:30 +0600

    instagram has been solved

---
+++ c/instagram.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int t, x, y;
+    scanf("%d", &t);
+    
+    while (t--) {
+        scanf("%d %d", &x, &y);
+        
+        if (x > 10 * y) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+    
+    return 0;
+}
