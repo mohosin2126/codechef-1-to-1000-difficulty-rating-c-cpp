ae4d3bcc854fe04142e72df5dbcddbe50c511d1e
mohosin2126 <mohosin2126@gmail.com>
2024-12-25 22:44:58 +0600

    monthly budget has been solved

---
+++ c/monthly-budget.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int t, x, y;
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d %d", &x, &y);
+        if (x >= 30 * y)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
