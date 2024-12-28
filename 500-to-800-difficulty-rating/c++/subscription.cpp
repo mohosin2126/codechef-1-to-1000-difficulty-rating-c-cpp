fc6472c3003d7c771d3c5e186bb6966d2d25fa4c
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2024-12-28 22:18:53 +0600

    subscription has been solved

--- 500-to-800-difficulty-rating/c++/subscription.cpp
+++ 500-to-800-difficulty-rating/c++/subscription.cpp
@@ -1,1 +1,18 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int subscriptions = (n + 5) / 6;
+        int total_cost = subscriptions * x;
+        cout << total_cost << endl;
+    }
+
+    return 0;
+}
