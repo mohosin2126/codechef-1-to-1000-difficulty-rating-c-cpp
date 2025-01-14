a1f79380aa60e94839ad8b45410bed8dca6b8650
mohosin2126 <mohosin2126@gmail.com>
2025-01-14 18:28:48 +0600

    expert setter has been solved

---
+++ 500-to-800-difficulty-rating/c++/expert-setter.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        if (y * 2 >= x)
+        {
+            cout << "YES" << "\n";
+        }
+        else
+        {
+            cout << "NO" << "\n";
+        }
+    }
+    return 0;
+}
