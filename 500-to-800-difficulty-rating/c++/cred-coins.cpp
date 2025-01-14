a1384d3799df09210a5e82d4f5d384d22fc7065d
mohosin2126 <mohosin2126@gmail.com>
2025-01-14 18:20:42 +0600

    cred coins has been solved

---
+++ 500-to-800-difficulty-rating/c++/cred-coins.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        int total_coins = x * y;
+        int bags = total_coins / 100;
+        cout << bags << "\n";
+    }
+    return 0;
+}
