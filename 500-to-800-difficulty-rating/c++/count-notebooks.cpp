b23288eb0c483b56d45c20ba1d57dced78bee320
mohosin2126 <mohosin2126@gmail.com>
2025-01-16 14:18:57 +0600

    count notebook has been solved

---
+++ 500-to-800-difficulty-rating/c++/count-notebooks.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int notebooks = (n * 1000) / 100;
+        cout << notebooks << endl;
+    }
+    return 0;
+}
