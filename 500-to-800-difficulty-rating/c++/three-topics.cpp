132dd49f8d134b9136ee1d7757f52980e635041f
mohosin2126 <mohosin2126@gmail.com>
2025-01-17 22:10:24 +0600

    three topics has been solved

---
+++ 500-to-800-difficulty-rating/c++/three-topics.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+
+    if (x == a || x == b || x == c)
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+
+    return 0;
+}
