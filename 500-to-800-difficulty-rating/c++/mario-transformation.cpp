776a45e817461a152f4f75616e98562d5def4415
mohosin2126 <mohosin2126@gmail.com>
2025-01-24 23:02:18 +0600

    mario transformation has been solved

---
+++ 500-to-800-difficulty-rating/c++/mario-transformation.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int X;
+        cin >> X;
+
+        int state = X % 3;
+        if (state == 0)
+        {
+            cout << "NORMAL" << endl;
+        }
+        else if (state == 1)
+        {
+            cout << "HUGE" << endl;
+        }
+        else if (state == 2)
+        {
+            cout << "SMALL" << endl;
+        }
+    }
+
+    return 0;
+}
