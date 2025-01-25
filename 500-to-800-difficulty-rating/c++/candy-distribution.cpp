b3f9b82c2dc21459384368b413ee60fd92b45355
mohosin2126 <mohosin2126@gmail.com>
2025-01-25 22:51:22 +0600

    candy distribution has been solved

---
+++ 500-to-800-difficulty-rating/c++/candy-distribution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N, M;
+        cin >> N >> M;
+
+        if (N % M == 0 && (N / M) % 2 == 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
+}
