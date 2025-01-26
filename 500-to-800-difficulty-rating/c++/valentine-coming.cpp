1428aefa0d53fb438dea6bcfe0be7e7c63c31f0d
mohosin2126 <mohosin2126@gmail.com>
2025-01-26 23:32:10 +0600

    valentine is coming has been solved

---
+++ 500-to-800-difficulty-rating/c++/valentine-coming.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, x, y;
+    cin >> t;
+    while (t--)
+    {
+        cin >> x >> y;
+        cout << x / y << "\n";
+    }
+    return 0;
+}
