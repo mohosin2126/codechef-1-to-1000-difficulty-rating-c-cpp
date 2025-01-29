fa5e0ead4fa8d7b295af6ab51ae8a2424e5b9f55
mohosin2126 <mohosin2126@gmail.com>
2025-01-29 23:55:15 +0600

    min number of coins has been solved

---
+++ 500-to-800-difficulty-rating/c++/min-coins.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        if (x % 5 != 0)
+        {
+            cout << "-1\n";
+        }
+        else
+        {
+            cout << (x / 10) + ((x % 10) / 5) << "\n";
+        }
+    }
+    return 0;
+}
