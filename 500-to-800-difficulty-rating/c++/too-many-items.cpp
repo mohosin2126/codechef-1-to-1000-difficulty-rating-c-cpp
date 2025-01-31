0244334aa67de41280d80093d434c6da4490e8de
mohosin2126 <mohosin2126@gmail.com>
2025-01-31 21:52:40 +0600

    too many items has been solved

---
+++ 500-to-800-difficulty-rating/c++/too-many-items.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        int polybags = (N + 9) / 10;
+
+        cout << polybags << endl;
+    }
+    return 0;
+}
