4444c5b1253711c34387db2178382ff5ea58b2aa
mohosin2126 <mohosin2126@gmail.com>
2025-02-01 21:21:09 +0600

    factorial has been solved

---
+++ 500-to-800-difficulty-rating/c++/factorial.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long factorial(int n)
+{
+    long long fact = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << factorial(N) << endl;
+    }
+    return 0;
+}
