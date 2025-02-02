146016979c12773c3bd1f27cda5054c48f397d92
mohosin2126 <mohosin2126@gmail.com>
2025-02-02 23:47:10 +0600

    chef and operators has been solved

---
+++ 500-to-800-difficulty-rating/c++/chef-operators.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int A, B;
+        cin >> A >> B;
+
+        if (A > B)
+            cout << ">" << endl;
+        else if (A < B)
+            cout << "<" << endl;
+        else
+            cout << "=" << endl;
+    }
+
+    return 0;
+}
