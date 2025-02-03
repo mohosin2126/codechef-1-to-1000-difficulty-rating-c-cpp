4b35229ec3b7a297fb5839367bdaf6656476c3be
mohosin2126 <mohosin2126@gmail.com>
2025-02-03 23:29:59 +0600

    reach fast has been solved

---
+++ 500-to-800-difficulty-rating/c++/reach-fast.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, K;
+        cin >> A >> B >> K;
+        int distance = abs(A - B);
+        int steps = (distance + K - 1) / K;
+        cout << steps << endl;
+    }
+    return 0;
+}
